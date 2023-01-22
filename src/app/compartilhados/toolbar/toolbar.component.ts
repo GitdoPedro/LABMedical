dfa7c972ecaa0d120ad2cd7c8d41eb16e4e526d9
GitdoPedro <emailpedrolima@gmail.com>
2023-01-22 20:00:32 -0300

    implementado o toolbar

--- src/app/compartilhados/toolbar/toolbar.component.ts
+++ src/app/compartilhados/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
+  @Input() titulo: string = 'PÁGINA DE TESTE';
+  @Input() user: string = 'admin';
 
 }
